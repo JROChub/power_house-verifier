ec9db408449b40853226b2fc930064b0b4ae4239
Lex Luger <jpmessage@icloud.com>
2026-05-19 22:18:35 -0700

    Add powerhouse/__init__.py

---
+++ powerhouse/__init__.py
@@ -0,0 +1,31 @@
+from .core import (
+    ClosureParams,
+    ScheduleStep,
+    ClosureResult,
+    per_participant,
+    per_round,
+    pr_finalize,
+    expected_rounds,
+    recommend_schedule,
+    closure_result,
+    required_rounds,
+    explain,
+    demo_sextillion_scale,
+)
+
+__version__ = "1.0.0"
+
+__all__ = [
+    "ClosureParams",
+    "ScheduleStep",
+    "ClosureResult",
+    "per_participant",
+    "per_round",
+    "pr_finalize",
+    "expected_rounds",
+    "recommend_schedule",
+    "closure_result",
+    "required_rounds",
+    "explain",
+    "demo_sextillion_scale",
+]
